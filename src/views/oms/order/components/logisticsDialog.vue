0bff6e870664fe145c75ca24a0b04a208592e094
yujie <yujie@richinfo.cn>
2024-02-19 00:31:23 +0800

    update

--- src/views/oms/order/components/logisticsDialog.vue
+++ src/views/oms/order/components/logisticsDialog.vue
@@ -17,15 +17,6 @@
 <script>
 import {fetchList} from '@/api/delivery';
 
-const defaultLogisticsList = [
-  {name: '订单已提交，等待付款', time: '2017-04-01 12:00:00 '},
-  {name: '订单付款成功', time: '2017-04-01 12:00:00 '},
-  {name: '在北京市进行下级地点扫描，等待付款', time: '2017-04-01 12:00:00 '},
-  {name: '在分拨中心广东深圳公司进行卸车扫描，等待付款', time: '2017-04-01 12:00:00 '},
-  {name: '在广东深圳公司进行发出扫描', time: '2017-04-01 12:00:00 '},
-  {name: '到达目的地网点广东深圳公司，快件将很快进行派送', time: '2017-04-01 12:00:00 '},
-  {name: '订单已签收，期待再次为您服务', time: '2017-04-01 12:00:00 '}
-];
 export default {
   name: 'logisticsDialog',
   props: {
